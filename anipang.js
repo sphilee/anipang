5e1e34bc185dbc5d70cfe738436603091ebb48e6
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2017-12-22 10:28:43 +0900

    Update anipang.js

--- anipang.js
+++ anipang.js
@@ -1,58 +1,65 @@
 module.exports = class Anipang {
     constructor(arr) {
         this.arr = JSON.parse(JSON.stringify(arr));
-        this.length = arr.length;
+        this.rowLength = arr.length;
         this.isCheck = false;
     }
 
     startPang() {
         this.checkPang();
     }
-    printPang() {
-        console.log(this.arr);
-    }
-    transpose(arr) {
-        return arr[0].map((col, i) => arr.map(row => row[i]));
-    }
+
     checkPang() {
         this.isCheck = false;
         this.checkRow().checkCol().arrangePang();
         if (this.isCheck) this.checkPang();
     }
+
+    printPang() {
+        console.log(this.arr);
+    }
+
+    transpose(arr) {
+        return arr[0].map((col, i) => arr.map(row => row[i]));
+    }
+
     arrangePang() {
         this.arr = this.transpose(this.transpose(this.arr).map(row => {
             const newRow = row.filter(item => item !== 0);
-            let length = newRow.length;
-            while (length < this.length) {
+            let newRowLength = newRow.length;
+            while (newRowLength < this.rowLength) {
                 newRow.unshift(0);
-                length++;
+                newRowLength++;
             }
             return newRow;
         }));
         return this;
     }
+
     checkRow() {
         this.arr = this.arr.map(row => {
-            row.forEach((v, j, rowArr) => this.erasePang(v, j, rowArr));
+            row.forEach((v, i, rowArr) => this.erasePang(v, i, rowArr));
             return row;
         });
         return this;
     }
+
     checkCol() {
         this.arr = this.transpose(this.transpose(this.arr).map(col => {
-            col.forEach((v, j, colArr) => this.erasePang(v, j, colArr));
+            col.forEach((v, i, colArr) => this.erasePang(v, i, colArr));
             return col;
         }));
         return this;
     }
-    erasePang(v, j, Arr) {
+
+    erasePang(v, i, Arr) {
         let pangCnt = 1;
-        Arr.slice(j + 1).some(compareNum => {
+        Arr.slice(i + 1).some(compareNum => {
             if (compareNum === v && v !== 0) pangCnt++;
             return compareNum !== v;
         })
         if (pangCnt >= 3) {
-            Arr.splice(j, pangCnt, ...(new Array(pangCnt).fill(0)));
+            Arr.splice(i, pangCnt, ...(Array(pangCnt).fill(0)));
             this.isCheck = true;
         }
     }
